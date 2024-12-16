a93fcdd93f00002dba68c8d8c8e2f483d116a39c
Moisés-Olv <moissclauoli@gmail.com>
2024-12-16 03:02:41 -0400

    somente teste de novo

--- Moiss/moiss.py
+++ Moiss/moiss.py
@@ -1,6 +1,6 @@
 # Programação Orientada a Objetos - P.O.O
 # 2°B - Vespertino
-# Alunos: Ana Clara Rodrigues; Lívia Esteves de Oliveira; Moisés Claudino Oliveira; Sarha Sthefanny Araripe Silva
+# Alunos: Moisés Claudino Oliveira
 
 import os
 import re
